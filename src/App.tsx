363679bd02b72e3bbc7dfa2b28fc8352c7208693
Sanuthb <165442353+Sanuthb@users.noreply.github.com>
2024-10-05 07:07:57 +0530

    my first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl text-purple bg-yellow underline font-primary-font">
-      Hello world!
+      HELLO KISHAN THIS IS YOUR BRANCH
     </h1>
   )
 }
