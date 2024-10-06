8d209feabc448dcf187bac2d12e7711394cc28d0
Sanuthb <165442353+Sanuthb@users.noreply.github.com>
2024-10-06 17:43:09 +0530

    done

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,24 +1,24 @@
 import Hero from "../Components/Home/Hero/Hero"
 import About from "../Components/Home/About/About"
 import Service from "../Components/Home/Services/Service"
-import LocomotiveScroll from 'locomotive-scroll';
-import 'locomotive-scroll/dist/locomotive-scroll.css';
-import { useEffect, useRef } from "react";
+// import LocomotiveScroll from 'locomotive-scroll';
+// import 'locomotive-scroll/dist/locomotive-scroll.css';
+// import { useEffect, useRef } from "react";
 
 const Home = () => {
-  const scrollRef = useRef(null);
-  useEffect(() => {
-    const scroll = new LocomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
-    });
+  // const scrollRef = useRef(null);
+  // useEffect(() => {
+  //   const scroll = new LocomotiveScroll({
+  //     el: scrollRef.current,
+  //     smooth: true,
+  //   });
 
-    return () => {
-      scroll.destroy();
-    };
-  }, []);
+  //   return () => {
+  //     scroll.destroy();
+  //   };
+  // }, []);
   return (
-    <main className="overflow-x-hidden" data-scroll-container ref={scrollRef}>
+    <main className="overflow-x-hidden">
       <Hero/>
       <About/>
       <Service/>
