cc3b7f768d882fa79c4192358c667dc89500ea4c
Sanuthb <165442353+Sanuthb@users.noreply.github.com>
2024-10-20 09:01:55 +0530

    all done

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,14 @@
 import Hero from "../Components/Home/Hero/Hero";
 import About from "../Components/Home/About/About";
 import Service from "../Components/Home/Services/Service";
-// import Testimonial from "../Components/testimonial/Testimonial";
+import Testimonial from "../Components/testimonial/Testimonial";
 const Home: React.FC = () => {
   return (
         <main  className="overflow-x-hidden">
           <Hero />
           <About />
           <Service />
-          {/* <Testimonial/> */}
+          <Testimonial/>
         </main>
   );
 };
