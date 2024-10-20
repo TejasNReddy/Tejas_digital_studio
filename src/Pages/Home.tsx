a3fa3fe0acff7570db89a86bd6574d5d2c0b3fa6
Sanuthb <165442353+Sanuthb@users.noreply.github.com>
2024-10-20 09:22:44 +0530

    all done

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,7 @@
 import Hero from "../Components/Home/Hero/Hero";
 import About from "../Components/Home/About/About";
 import Service from "../Components/Home/Services/Service";
-import Testimonial from "../Components/testimonial/Testimonial";
+import Testimonial from "../Components/Testimonial_sec/Testimonial";
 const Home: React.FC = () => {
   return (
         <main  className="overflow-x-hidden">
